f9fd0454fb6c2903e3864bda3a2020feec1a5f51
overaction00 <overaction00@overaction00-PC.korea.ncsoft.corp>
2013-07-05 22:00:03 +0900

    정렬된 koreans 에서 lower_bound 메소드를 수현하여 찾아야 함

--- apss/src/chapter10/example2/matchorder/Main.java
+++ apss/src/chapter10/example2/matchorder/Main.java
@@ -1,9 +1,24 @@
 package chapter10.example2.matchorder;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 	
+	static class MatchOrder {
+		
+		public int order(Integer[] russians, Integer[] koreans) {
+			Arrays.sort(koreans, Collections.reverseOrder());
+			
+			for (int rus : russians) {
+				// 가장 레이팅이 높은 한국선수가 이길수 없다면 가장 레이팅이 낮은 사람을 매칭시킨다.
+				
+				// 한국 선수가 이길 수 있다면 동일하거나 하나위의 선수를 출선시킨다.
+			}
+			return 0;
+		}
+	}
 	
 	
 	public static void main(String[] args) {
