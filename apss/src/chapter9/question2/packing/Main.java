41a8c0bc673b29062f092b91f7138b246aef1916
overaction00 <overaction00@gmail.com>
2013-07-10 00:34:51 +0900

    책보고 따라함.. 이해안

--- apss/src/chapter9/question2/packing/Main.java
+++ apss/src/chapter9/question2/packing/Main.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
 	
 	public static final int MAX = 100;
@@ -16,46 +15,50 @@ public class Main {
 	private static int[] volume = new int[MAX + 1];
 	private static int[] need = new int[MAX + 1];
 	private static int[][] cache = new int[NEED_MAX + 1][MAX + 1];
-	
-	
 	static class Packing {
 		
 		private int n;
-		private int satisfiedMax;
-		private List<String> selectedItems = new ArrayList<String>();
 		
 		public Packing(int n) {
 			this.n = n;
 		}
 		
-		public int pack(int capacity, int item, int satisfied, List<String> names) {
+		public int pack(int capacity, int item) {
 			if (item == n) {
-				if (satisfiedMax <= satisfied) {
-					satisfiedMax = satisfied;
-					selectedItems = names;
-				}
-				return satisfied;
+				return 0;
 			}
 			
-			if ( cache[capacity][item] != -1 ) {
+			if ( cache[capacity][item] != -1) {
 				return cache[capacity][item];
 			}
+			
 			int pick = 0;
 			int nPick = 0;
 			// 물건을 가져가는 경우 (pick)
 			if ( capacity >= volume[item] ) {
-				List<String> newItems = new ArrayList<String>(names);
-				newItems.add(name[item]);
-				pick = pack(capacity - volume[item], item + 1, satisfied + need[item], newItems);
+				pick = pack(capacity - volume[item], item + 1) + need[item];
 			}
 			
 			// 물건을 가져가지 않는 경우 (nPick)
-			nPick = pack(capacity, item + 1, satisfied, names);
+			nPick = pack(capacity, item + 1);
 			
 			// 두가지 경우에 대해서 더 만족도가 큰 경우를 판단
 			int max = Math.max(pick, nPick);
 			cache[capacity][item] = max;
 			return max;
+		}
+		
+		public void reconstruct(int capacity, int item, List<String> picked) {
+			if (item == n) {
+				return;
+			}
+			
+			if (pack(capacity, item) == pack(capacity, item + 1)) {
+				reconstruct(capacity, item + 1, picked);
+			} else {
+				picked.add(name[item]);
+				reconstruct(capacity - volume[item], item + 1, picked);
+			}
 		}
 	}
 	
@@ -69,7 +72,6 @@ public class Main {
 			for (int j = 0; j < MAX + 1; j++) {
 				cache[i][j] = -1;
 			}
-			
 		}
 	}
 
@@ -89,11 +91,14 @@ public class Main {
 				need[i] = Integer.parseInt(arr[2]);
 			}
 			Packing p = new Packing(noItems);
-			int maxSatisfied = p.pack(capacity, 0, 0, new ArrayList<String>());
-			System.out.println(maxSatisfied + " " + p.selectedItems.size());
-			for (String item : p.selectedItems) {
+			int maxSatisfied = p.pack(capacity, 0);
+			List<String> picked = new ArrayList<String>();
+			p.reconstruct(capacity, 0, picked);
+			System.out.println(maxSatisfied + " " + picked.size());
+			for (String item : picked) {
 				System.out.println(item);
 			}
+		
 		}
 		sc.close();
 	}
